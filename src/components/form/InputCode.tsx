f9a27c9910f7c3d96db06bedaae13c86f6e3b7db
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2025-11-27 02:26:26 +0200

    Edit ICodeBtn

--- src/components/form/InputCode.tsx
+++ src/components/form/InputCode.tsx
@@ -1,20 +1,43 @@
+import { forwardRef, useState, type KeyboardEvent } from "react"
 
-type InputCodeProps = React.InputHTMLAttributes<HTMLInputElement>
-const InputCode = ({...rest}: InputCodeProps) => {
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement>{
+  next?: React.RefObject<HTMLInputElement | null>;
+  prev?: React.RefObject<HTMLInputElement | null>;
+}
+const InputCode = forwardRef<HTMLInputElement, IProps>(({ next, prev,...rest}: IProps, ref) => {
+  const [value, setValue] = useState<boolean>(false)
   const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
     const input = e.currentTarget
     input.value = input.value.replace(/[^0-9]/g,"")
+      if (input.value) {
+        setValue(true);
+      } else {
+        setValue(false);
+      }
+    if(input.value && next) {
+      next.current?.focus()
+    }
+  }
+  const handleKeyDown = (e : KeyboardEvent<HTMLInputElement>) => {
+    const input = e.currentTarget
+    if(e.key === "Backspace" && !input.value && prev) {
+      prev.current?.focus()
+    }
   }
   return (<>
       <input
+      ref={ref}
       {...rest}
       type="text"
       maxLength={1}
       pattern="[0-9]"
-      onInput={handleInput}
       inputMode="numeric"
-      className="w-14 h-15 text-center text-2xl font-semibold border-3 border-gray-400 rounded-xl transition-all duration-300 ease-in focus:border-primary-500 shadow-sm outline-none"/>
+      onInput={handleInput}
+      onKeyDown={handleKeyDown}
+      className={`w-14 h-14 text-center text-2xl font-semibold border-2 rounded-xl transition-all duration-300 focus:border-primary-500 ${rest.className}
+      ${value ? "bg-primary-100 outline-4 outline-gray-100 border-primary-500" : "border-gray-300 outline-none"}`} />
   </>)
 }
+)
 
 export default InputCode
--- src/pages/auth/ForgetPasswordVerify.tsx
+++ src/pages/auth/ForgetPasswordVerify.tsx
@@ -3,18 +3,31 @@ import FormTitle from "../../components/form/FormTitle"
 import AuthLayout from "../../layouts/AuthLayout"
 import CardAuth from "../../components/common/CardAuth"
 import Label from "../../components/common/Label"
+import { useEffect, useRef, useState } from "react"
 import InputCode from "../../components/form/InputCode"
+// import InputCode from "../../components/form/InputCode"
 
 const ForgetPassword = () => {
-    // const {
-    //     register,
-    //     handleSubmit,
-    //     formState: { errors },
-    //     } = useForm<IDataForgetPassword>({
-    //         resolver: yupResolver(ForgetPasswordSchema),
-    //     })
+  const [timer, setTimer] = useState<number>(60);
 
-    // const onSubmit = handleSubmit((data) => console.log(data))
+ console.log(timer)
+
+  const c1 = useRef<HTMLInputElement>(null);
+  const c2 = useRef<HTMLInputElement>(null);
+  const c3 = useRef<HTMLInputElement>(null);
+  const c4 = useRef<HTMLInputElement>(null);
+  const c5 = useRef<HTMLInputElement>(null);
+  const c6 = useRef<HTMLInputElement>(null);
+
+
+  //** Timar */
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((t) => (t > 0 ?  t - 1 : 0))
+    },1000)
+    return () => clearInterval(interval)
+  },[])
 
 return (<>
     <AuthLayout w="md:min-w-sm">
@@ -23,16 +36,17 @@ return (<>
         icon={<Mail size={36} className="text-white"/>}
         title="تحقق من بريدك الإلكتروني"
         des="أدخل الكود المكون من 6 أرقام الذي أرسلناه إلى:"/>
+        <p className="text-center text-primary-500">example@email.com</p>
         <form className="space-y-5 font-main">
           <div className="mt-6">
-            <Label text="كود التحقق" center="text-center"/>
+            <Label text="كود التحقق"/>
             <div className="flex gap-3 justify-center ltr">
-              <InputCode key={1} aria-label="Digit 1" autoFocus/>
-              <InputCode key={2} aria-label="Digit 2"/>
-              <InputCode key={3} aria-label="Digit 3"/>
-              <InputCode key={4} aria-label="Digit 4"/>
-              <InputCode key={5} aria-label="Digit 5"/>
-              <InputCode key={6} aria-label="Digit 6"/>
+              <InputCode ref={c1} next={c2}/>
+              <InputCode ref={c2} next={c3} prev={c1}/>
+              <InputCode ref={c3} next={c4} prev={c2}/>
+              <InputCode ref={c4} next={c5} prev={c3}/>
+              <InputCode ref={c5} next={c6} prev={c4}/>
+              <InputCode ref={c6} prev={c5}/>
             </div>
           </div>
 
