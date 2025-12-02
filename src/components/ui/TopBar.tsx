bc9fb7ef36a78ed4dcf5c2e3712adc0388e0a1da
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2025-12-02 03:29:02 +0200

    Add Topbar desgin

--- src/components/ui/TopBar.tsx
+++ src/components/ui/TopBar.tsx
@@ -1,7 +1,25 @@
+import { Calendar, Search } from "lucide-react"
+import Input from "../common/Input"
 
 const TopBar = () => {
   return (
-    <div>TopBar</div>
+    <div className="bg-white shadow-md border-b border-gray-200 px-8 py-5 sticky top-0 z-10 backdrop-blur-sm bg-opacity-95">
+      <div className="flex justify-between items-center space-x-40">
+        <div className=" flex-1">
+          <Input type="text" placeholder="بحث سريع في النظام..." name="search" icon={<Search size={20}/>} pl="pl-4"/>
+        </div>
+        <div className="flex items-center gap-4">
+          <button className="p-3 hover:bg-gray-100 rounded-xl transition-all">
+            <Calendar size={22} className="text-gray-600" />
+          </button>
+          <div className="h-8 w-px bg-gray-300 mx-2"></div>
+          <div className="text-right">
+            <p className="text-sm font-bold text-gray-800">الجمعة</p>
+            <p className="text-xs text-gray-500">14 نوفمبر 2025</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,23 +1,29 @@
 import { PanelGroup, Panel} from "react-resizable-panels"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import SideRightBar from "../components/ui/SideRightBar"
 import TopBar from "../components/ui/TopBar"
 import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 780);
   const [sidebarOpen , setSidebarOpen ] = useState<boolean>(true)
+
+
+  useEffect(() => {
+    const handleResize = () => setIsSmallScreen(window.innerWidth <= 991);
+    window.addEventListener("resize", handleResize);
+  return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="flex h-screen w-full overflow-hidden">
       <div className="flex-1 flex flex-col">
         <PanelGroup direction="horizontal" className="flex-1">
-          <Panel minSize={sidebarOpen ? 15 : 5} maxSize={sidebarOpen ? 15 : 5} className="text-white transition-all duration-300 flex flex-col shadow-2xl" style={{background: "var(--gradient-dark)"}}>
+          <Panel minSize={isSmallScreen ? 30 : sidebarOpen ? 15 : 5} maxSize={isSmallScreen ? 30 : sidebarOpen ? 15 : 5} className="text-white transition-all duration-300 flex flex-col shadow-2xl" style={{background: "var(--gradient-dark)"}}>
             <SideRightBar sidebarOpen={sidebarOpen} onClickMenu={() => setSidebarOpen(prev => !prev)}/>
           </Panel>
           <Panel defaultSize={80} minSize={20} className="bg-gray-50 flex flex-col overflow-hidden">
-            <div className="h-16 border-b bg-white shadow-sm shrink-0">
-              <TopBar />
-            </div>
+            <TopBar />
             <div className="flex-1 overflow-auto p-4">
               <Outlet />
             </div>
