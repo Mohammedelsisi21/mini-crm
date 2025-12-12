50ae7c5925bc27862eb96d1d9864fbf0318a3e36
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2025-12-12 17:26:15 +0200

    Updates

---
+++ assets/Label-DMdW_9Md.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-cOkxID9w.js";const l=({text:e,center:s,htmlFor:o})=>t.jsx(t.Fragment,{children:t.jsx("label",{className:`block text-sm font-bold text-text-body mb-2 ${s}`,htmlFor:o,children:e})});export{l as L};
